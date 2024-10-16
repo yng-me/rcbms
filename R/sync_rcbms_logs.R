afcf76468cb3f2ce9fe3a97d5786e6728edaba2d
Bhas Abdulsamad <aeabdulsamad@gmail.com>
2024-10-16 16:56:41 +0800

    sync logs

---
+++ R/sync_rcbms_logs.R
@@ -0,0 +1,33 @@
+#' Title
+#'
+#' @param path
+#' @param db_dir
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+
+sync_rcbms_logs <- function(db_dir, path = '//localhost/webdav') {
+
+  rcbms_db_files <- list.files(
+    path,
+    recursive = T,
+    pattern = '(hp|shp|ilq|bp)_rcbms_logs_.*\\.db',
+    full.names = T
+  )
+
+  if(length(rcbms_db_files) == 0) {
+    return(cat('empty'))
+  }
+
+  for(i in seq_along(rcbms_db_files)) {
+    extract_rcbms_log(db_dir, rcbms_db_files[i])
+  }
+
+  unlink(rcbms_db_files, recursive = T, force = T)
+
+  cat('success')
+
+}
